3d3fc306ab2afaf551fb4fd7429ad0d24bda977f
Steve Perkins <stereosteve@users.noreply.github.com>
2025-03-28 09:53:47 -0400

    csv driven fixtures... is rad.

---
+++ api/fixture_test.go
@@ -0,0 +1,118 @@
+package api
+
+import (
+	"context"
+	"encoding/csv"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var (
+	userBaseRow = map[string]any{
+		"user_id":              nil,
+		"handle":               nil,
+		"handle_lc":            nil,
+		"is_current":           true,
+		"is_verified":          false,
+		"created_at":           time.Now(),
+		"updated_at":           time.Now(),
+		"has_collectibles":     false,
+		"txhash":               "tx1",
+		"is_deactivated":       false,
+		"is_available":         true,
+		"is_storage_v2":        false,
+		"allow_ai_attribution": false,
+	}
+
+	trackBaseRow = map[string]any{
+		"blockhash":                             "block_abc123",
+		"track_id":                              "@track_id",
+		"is_current":                            true,
+		"is_delete":                             false,
+		"owner_id":                              "@owner_id",
+		"title":                                 "@title",
+		"genre":                                 "Electronic",
+		"mood":                                  "Energetic",
+		"created_at":                            time.Now(),
+		"updated_at":                            time.Now(),
+		"txhash":                                "tx_123abc",
+		"is_unlisted":                           false,
+		"is_available":                          true,
+		"track_segments":                        "[]", // JSONB string
+		"is_scheduled_release":                  false,
+		"is_downloadable":                       false,
+		"is_original_available":                 false,
+		"playlists_containing_track":            "{}", // JSONB string
+		"playlists_previously_containing_track": map[string]any{},
+		"audio_analysis_error_count":            0,
+		"is_owned_by_user":                      false,
+	}
+
+	followBaseRow = map[string]any{
+		"blockhash":        "block1",
+		"blocknumber":      101,
+		"follower_user_id": nil,
+		"followee_user_id": nil,
+		"is_current":       true,
+		"is_delete":        false,
+		"created_at":       time.Now(),
+		"txhash":           "tx123",
+		"slot":             500,
+	}
+
+	repostBaseRow = map[string]any{
+		"blockhash":           "block_abc123",
+		"blocknumber":         101,
+		"user_id":             nil,
+		"repost_item_id":      nil,
+		"repost_type":         nil,
+		"is_current":          true,
+		"is_delete":           false,
+		"created_at":          time.Now(),
+		"txhash":              "tx_456def",
+		"slot":                500,
+		"is_repost_of_repost": false,
+	}
+)
+
+func insertFixtures(table string, baseRow map[string]any, csvFile string) {
+	file, err := os.Open(csvFile)
+	checkErr(err)
+	defer file.Close()
+
+	fieldList := []string{}
+	for f := range baseRow {
+		fieldList = append(fieldList, f)
+	}
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	checkErr(err)
+
+	var records [][]any
+
+	csvHeader := rows[0]
+	for _, row := range rows[1:] {
+		for i, field := range csvHeader {
+			if row[i] != "" {
+				baseRow[field] = row[i]
+			}
+		}
+
+		vals := []any{}
+		for _, field := range fieldList {
+			vals = append(vals, baseRow[field])
+		}
+		records = append(records, vals)
+	}
+
+	_, err = app.conn.CopyFrom(
+		context.Background(),
+		pgx.Identifier{table},
+		fieldList,
+		pgx.CopyFromRows(records),
+	)
+	checkErr(err)
+}
--- api/server_test.go
+++ api/server_test.go
@@ -17,14 +17,15 @@ var (
 	app *ApiServer
 )
 
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestMain(m *testing.M) {
 	ctx := context.Background()
-
-	checkErr := func(err error) {
-		if err != nil {
-			panic(err)
-		}
-	}
+	var err error
 
 	// create a test db from template
 	{
@@ -43,143 +44,9 @@ func TestMain(m *testing.M) {
 	})
 
 	// seed db
-	tx, err := app.conn.Begin(ctx)
-	checkErr(err)
-
-	userFixtures := []struct {
-		user_id    int
-		handle     string
-		deactivate bool
-	}{
-		{
-			user_id: 1,
-			handle:  "rayjacobson",
-		},
-		{
-			user_id: 2,
-			handle:  "stereosteve",
-		},
-		{
-			user_id:    91,
-			handle:     "badguy",
-			deactivate: true,
-		},
-	}
-	for _, u := range userFixtures {
-		_, err = tx.Exec(ctx, `
-		INSERT INTO public.users (
-			user_id,
-			handle,
-			handle_lc,
-			is_current,
-			is_verified,
-			created_at,
-			updated_at,
-			has_collectibles,
-			txhash,
-			is_deactivated,
-			is_available,
-			is_storage_v2,
-			allow_ai_attribution
-		) VALUES (
-			@id,
-			@handle,
-			@handle_lc,
-			true,                 -- is_current
-			false,                -- is_verified
-			CURRENT_TIMESTAMP,    -- created_at
-			CURRENT_TIMESTAMP,    -- updated_at
-			false,                -- has_collectibles
-			'',                   -- txhash
-			@deactivate,                -- is_deactivated
-			true,                 -- is_available
-			false,                -- is_storage_v2
-			false                 -- allow_ai_attribution
-		)`, pgx.NamedArgs{
-			"id":         u.user_id,
-			"handle":     u.handle,
-			"handle_lc":  strings.ToLower(u.handle),
-			"deactivate": u.deactivate,
-		})
-		checkErr(err)
-	}
-
-	// track fixtures
-	trackFixtures := []struct {
-		id          int
-		owner_id    int
-		title       string
-		is_unlisted bool
-	}{
-		{
-			id:       200,
-			owner_id: 2,
-			title:    "Culca Canyon",
-		},
-		{
-			id:          201,
-			owner_id:    2,
-			title:       "Turkey Time DEMO",
-			is_unlisted: true,
-		},
-	}
-	for _, t := range trackFixtures {
-		_, err := tx.Exec(ctx, `
-		INSERT INTO public.tracks (
-			blockhash,
-			track_id,
-			is_current,
-			is_delete,
-			owner_id,
-			title,
-			genre,
-			mood,
-			created_at,
-			updated_at,
-			txhash,
-			is_unlisted,
-			is_available,
-			track_segments,
-			is_scheduled_release,
-			is_downloadable,
-			is_original_available,
-			playlists_containing_track,
-			playlists_previously_containing_track,
-			audio_analysis_error_count,
-			is_owned_by_user
-		) VALUES (
-			'block_abc123',                    -- blockhash
-			@track_id,                              -- track_id
-			true,                              -- is_current
-			false,                             -- is_delete
-			@owner_id,                                 -- owner_id
-			@title,                  -- title
-			'Electronic',                      -- genre
-			'Energetic',                       -- mood
-			CURRENT_TIMESTAMP,                 -- created_at
-			CURRENT_TIMESTAMP,                 -- updated_at
-			'tx_123abc',                       -- txhash
-			@is_unlisted,                             -- is_unlisted
-			true,                              -- is_available
-			'[]'::jsonb,                       -- track_segments
-			false,                             -- is_scheduled_release
-			false,                             -- is_downloadable
-			false,                             -- is_original_available
-			'{}',                              -- playlists_containing_track
-			jsonb_build_object(),              -- playlists_previously_containing_track
-			0,                                 -- audio_analysis_error_count
-			false                              -- is_owned_by_user
-		);`, pgx.NamedArgs{
-			"track_id":    t.id,
-			"owner_id":    t.owner_id,
-			"title":       t.title,
-			"is_unlisted": t.is_unlisted,
-		})
-		checkErr(err)
-	}
 
 	// stupid block fixture
-	_, err = tx.Exec(ctx, `
+	_, err = app.conn.Exec(ctx, `
 	INSERT INTO public.blocks (
 		blockhash,
 		parenthash,
@@ -188,91 +55,16 @@ func TestMain(m *testing.M) {
 	) VALUES (
 		'block1',   -- blockhash
 		'block0',   -- parenthash
-		true,             -- is_current
-		101               -- number
+		true,
+		101
 	);
 	`)
 	checkErr(err)
 
-	// follow fixtures
-	followFixtures := []struct {
-		actorId  int
-		targetId int
-	}{
-		{1, 2},
-		{2, 1},
-	}
-	for _, f := range followFixtures {
-		_, err = tx.Exec(ctx, `
-		INSERT INTO public.follows (
-			blockhash,
-			blocknumber,
-			follower_user_id,
-			followee_user_id,
-			is_current,
-			is_delete,
-			created_at,
-			txhash,
-			slot
-		) VALUES (
-			'block1',         -- blockhash
-			101,              -- blocknumber
-			$1,                -- follower_user_id
-			$2,                -- followee_user_id
-			true,             -- is_current
-			false,            -- is_delete
-			CURRENT_TIMESTAMP,-- created_at
-			'tx123',          -- txhash
-			500               -- slot
-		);
-		`, f.actorId, f.targetId)
-		checkErr(err)
-	}
-
-	// reposts
-	repostFixtures := []struct {
-		user_id        int
-		repost_type    string
-		repost_item_id int
-	}{
-		{1, "track", 200},
-	}
-	for _, r := range repostFixtures {
-		_, err := tx.Exec(ctx, `
-		INSERT INTO public.reposts (
-			blockhash,
-			blocknumber,
-			user_id,
-			repost_item_id,
-			repost_type,
-			is_current,
-			is_delete,
-			created_at,
-			txhash,
-			slot,
-			is_repost_of_repost
-		) VALUES (
-			'block_abc123',       -- blockhash
-			101,                  -- blocknumber
-			@user_id,                   -- user_id
-			@repost_item_id,                 -- repost_item_id (e.g., track ID or playlist ID)
-			@repost_type,              -- repost_type (must be a valid value from reposttype enum)
-			true,                 -- is_current
-			false,                -- is_delete
-			CURRENT_TIMESTAMP,    -- created_at
-			'tx_456def',          -- txhash
-			500,                  -- slot
-			false                 -- is_repost_of_repost
-		);
-		`, pgx.NamedArgs{
-			"user_id":        r.user_id,
-			"repost_type":    r.repost_type,
-			"repost_item_id": r.repost_item_id,
-		})
-		checkErr(err)
-	}
-
-	checkErr(tx.Commit(ctx))
+	insertFixtures("users", userBaseRow, "testdata/user_fixtures.csv")
+	insertFixtures("tracks", trackBaseRow, "testdata/track_fixtures.csv")
+	insertFixtures("follows", followBaseRow, "testdata/follow_fixtures.csv")
+	insertFixtures("reposts", repostBaseRow, "testdata/repost_fixtures.csv")
 
 	code := m.Run()
 
