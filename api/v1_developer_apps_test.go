01794fddaf67ea4dd0e70b605f36266984fed634
Steve Perkins <stereosteve@users.noreply.github.com>
2025-04-04 09:01:47 -0400

    Add playlist endpoint test

--- api/v1_developer_apps_test.go
+++ api/v1_developer_apps_test.go
@@ -19,8 +19,12 @@ func TestGetDeveloperAppsQueries(t *testing.T) {
 }
 
 func TestGetDeveloperApp(t *testing.T) {
-	status, body := testGet(t, "/v1/developer_apps/0x7d7b6b7a97d1deefe3a1ccc5a13c48e8f055e0b6")
+	var resp struct {
+		Data dbv1.FullDeveloperApp
+	}
+	status, body := testGet(t, "/v1/developer_apps/0x7d7b6b7a97d1deefe3a1ccc5a13c48e8f055e0b6", &resp)
 	assert.Equal(t, 200, status)
 	assert.True(t, strings.Contains(string(body), `"user_id":"7eP5n"`))
-	assert.True(t, strings.Contains(string(body), `"name": "create-audius-app",`))
+	assert.True(t, strings.Contains(string(body), `"name":"cool app"`))
+	assert.Equal(t, "cool app", resp.Data.Name)
 }
