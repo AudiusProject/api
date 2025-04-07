2f282316b23d346ff5d59c68f7bd089bffda10bf
Raymond Jacobson <ray@audius.co>
2025-04-07 12:48:39 -0700

    Add custom upstream to deliver middleware log

---
+++ api/upstream.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"github.com/valyala/fasthttp"
+)
+
+type roundrobin struct {
+	sync.Mutex
+
+	current int
+	pool    []string
+}
+
+// this method will return a string of addr server from list server.
+func (r *roundrobin) get() string {
+	r.Lock()
+	defer r.Unlock()
+
+	if r.current >= len(r.pool) {
+		r.current %= len(r.pool)
+	}
+
+	result := r.pool[r.current]
+	r.current++
+	return result
+}
+
+// Modified BalancerForward to add upstream server to fiber context
+func BalancerForward(servers []string, clients ...*fasthttp.Client) fiber.Handler {
+	r := &roundrobin{
+		current: 0,
+		pool:    servers,
+	}
+	return func(c *fiber.Ctx) error {
+		server := r.get()
+		if !strings.HasPrefix(server, "http") {
+			server = "http://" + server
+		}
+		c.Locals("upstream", server)
+		c.Request().Header.Add("X-Real-IP", c.IP())
+		return proxy.Do(c, server+c.OriginalURL(), clients...)
+	}
+}
