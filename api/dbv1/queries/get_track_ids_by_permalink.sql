328b96415317e5fa5659a7fb9b34d0c16ebe194f
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-04-24 15:14:18 -0700

    tabs

--- api/dbv1/queries/get_track_ids_by_permalink.sql
+++ api/dbv1/queries/get_track_ids_by_permalink.sql
@@ -4,5 +4,5 @@ FROM track_routes
 JOIN users ON users.user_id = track_routes.owner_id
 WHERE handle_lc = ANY(@handles::text[])
     AND slug = ANY(@slugs::text[])
-	AND CONCAT(handle_lc, '/', slug) = ANY(@permalinks::text[]) -- in case of conflicts across users
+    AND CONCAT(handle_lc, '/', slug) = ANY(@permalinks::text[]) -- in case of conflicts across users
 ;
