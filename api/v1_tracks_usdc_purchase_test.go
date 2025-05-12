d6e7b8f2bbda4c694e97a4613a18b92dacc56a74
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-05-12 16:14:59 -0400

    tests

---
+++ api/v1_tracks_usdc_purchase_test.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"testing"
+
+	"bridgerton.audius.co/api/dbv1"
+	"bridgerton.audius.co/trashid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUsdcPurchase(t *testing.T) {
+	var resp struct {
+		Data []dbv1.FullTrack
+	}
+	status, body := testGet(t, "/v1/tracks/usdc-purchase", &resp)
+
+	assert.Equal(t, 200, status)
+
+	jsonAssert(t, body, map[string]any{
+		"data.0.id":    trashid.MustEncodeHashID(506),
+		"data.0.genre": "Pop",
+
+		"data.1.id":    trashid.MustEncodeHashID(507),
+		"data.1.genre": "Jazz",
+
+		"data.2.id":    trashid.MustEncodeHashID(508),
+		"data.2.genre": "Classical",
+	})
+}
