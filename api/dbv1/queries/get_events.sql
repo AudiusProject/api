bfcd721638609f9378fd47c1c60249eea439c62f
Raymond Jacobson <ray@audius.co>
2025-05-20 14:13:40 -0400

    Fix event support

--- api/dbv1/queries/get_events.sql
+++ api/dbv1/queries/get_events.sql
@@ -13,6 +13,7 @@ SELECT
 FROM events
 WHERE
   (@entity_ids::int[] = '{}' OR entity_id = ANY(@entity_ids::int[]))
+  AND (@event_ids::int[] = '{}' OR event_id = ANY(@event_ids::int[]))
   AND (@entity_type::text = '' OR entity_type = @entity_type::event_entity_type)
   AND (@event_type::text = '' OR event_type = @event_type::event_type)
   AND (@filter_deleted::boolean IS NULL OR is_deleted = @filter_deleted)
