9d42de899e04373989b6da39aaaf6141dd9ce3fc
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-06-02 16:27:02 -0700

    fix tests

--- api/testdata/playlist_routes_fixtures.go
+++ api/testdata/playlist_routes_fixtures.go
@@ -2,6 +2,34 @@ package testdata
 
 var PlaylistRoutesFixtures = []map[string]any{
 	{
+		"slug":         "first",
+		"title_slug":   "first",
+		"collision_id": 0,
+		"owner_id":     1,
+		"playlist_id":  1,
+	},
+	{
+		"slug":         "follow-gated-stream",
+		"title_slug":   "follow-gated-stream",
+		"collision_id": 0,
+		"owner_id":     3,
+		"playlist_id":  2,
+	},
+	{
+		"slug":         "second-album",
+		"title_slug":   "second-album",
+		"collision_id": 0,
+		"owner_id":     1,
+		"playlist_id":  3,
+	},
+	{
+		"slug":         "purchase-gated-stream",
+		"title_slug":   "purchase-gated-stream",
+		"collision_id": 0,
+		"owner_id":     3,
+		"playlist_id":  4,
+	},
+	{
 		"slug":         "playlist-by-permalink",
 		"title_slug":   "playlist-by-permalink",
 		"collision_id": 0,
