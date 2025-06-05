69454fa52ab38872ab12a699ddca5f33f647bbac
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-06-05 16:58:58 -0700

    Add block_confirmation

---
+++ api/block_confirmation.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+)
+
+type BlockConfirmationQueryParams struct {
+	BlockHash   string `query:"blockhash"`
+	BlockNumber int64  `query:"blocknumber"`
+}
+
+func (app *ApiServer) BlockConfirmation(c *fiber.Ctx) error {
+	var params BlockConfirmationQueryParams
+	err := app.ParseAndValidateQueryParams(c, &params)
+	if err != nil {
+		return err
+	}
+
+	sql := `
+	SELECT
+		((SELECT TRUE FROM core_blocks WHERE chain_id = @chainId::text AND height > @blockNumber::bigint LIMIT 1) IS NOT NULL) AS block_passed,
+		((SELECT TRUE AS block_found FROM core_blocks WHERE hash = @blockHash::text AND chain_id = @chainId::text LIMIT 1) IS NOT NULL) AS block_found
+	;`
+	rows, err := app.pool.Query(c.Context(), sql, pgx.NamedArgs{
+		"blockHash":   params.BlockHash,
+		"blockNumber": params.BlockNumber,
+		"chainId":     "audius-mainnet-alpha-beta",
+	})
+	if err != nil {
+		return err
+	}
+
+	type BlockConfirmationResult struct {
+		BlockFound  bool `db:"block_found" json:"block_found"`
+		BlockPassed bool `db:"block_passed" json:"block_passed"`
+	}
+	res, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[BlockConfirmationResult])
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"data": res,
+	})
+}
