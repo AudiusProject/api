59fb01cee43c066e0a70e494d26902d10ab07b05
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-06-08 22:05:07 -0400

    disable handle cache (#127)

--- api/server.go
+++ api/server.go
@@ -485,15 +485,15 @@ func (app *ApiServer) paramTimeRange(c *fiber.Ctx, param string, defaultValue st
 	return timeRange, nil
 }
 
-func (app *ApiServer) resolveUserHandleToId(handle string) (int32, error) {
-	if hit, ok := app.resolveHandleCache.Get(handle); ok {
-		return hit, nil
-	}
-	user_id, err := app.queries.GetUserForHandle(context.Background(), handle)
+func (app *ApiServer) resolveUserHandleToId(c *fiber.Ctx, handle string) (int32, error) {
+	// if hit, ok := app.resolveHandleCache.Get(handle); ok {
+	// 	return hit, nil
+	// }
+	user_id, err := app.queries.GetUserForHandle(c.Context(), handle)
 	if err != nil {
 		return 0, err
 	}
-	app.resolveHandleCache.Set(handle, int32(user_id))
+	// app.resolveHandleCache.Set(handle, int32(user_id))
 	return int32(user_id), nil
 }
 
