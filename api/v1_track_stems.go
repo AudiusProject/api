867d4a370caa7e7eef1f714eec7145fdc45c13ba
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-06-12 15:58:31 -0400

    remove extraneous comment

--- api/v1_track_stems.go
+++ api/v1_track_stems.go
@@ -6,32 +6,6 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-/*def stem_from_track(track):
-  track_id = encode_int_id(track["track_id"])
-  parent_id = encode_int_id(track["stem_of"]["parent_track_id"])
-  category = track["stem_of"]["category"]
-  orig_filename = track["orig_filename"]
-  return {
-      "id": track_id,
-      "parent_id": parent_id,
-      "category": category,
-      "cid": track["track_cid"],
-      "user_id": encode_int_id(track["owner_id"]),
-      "orig_filename": orig_filename,
-      "blocknumber": track["blocknumber"],
-  }*/
-
-/*
-	{
-	    "id": fields.String(required=True),
-	    "parent_id": fields.String(required=True),
-	    "category": fields.String(required=True),
-	    "cid": fields.String(required=True),
-	    "user_id": fields.String(required=True),
-	    "blocknumber": fields.Integer(required=True),
-	    "orig_filename": fields.String(required=True),
-	},
-*/
 type TrackStem struct {
 	Id           trashid.HashId `db:"track_id" json:"id"`
 	ParentId     trashid.HashId `db:"parent_track_id" json:"parent_id"`
