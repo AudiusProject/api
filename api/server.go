0094fd4b4915658094bad9afaf416d38086a1449
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-06-16 18:55:16 -0400

    [API-190] Add track remix parents endpoint (#140)
    I'm matching the logic from the python endpoint, which is a little
    strange.
    * It is capable of returning multiple remix parents (though no prod data
    actually does this)
    * It does not support returning remix parents for premium tracks (you
    can't monetize a remix)
    * It does not support returning hidden remix parents (regardless if you
    own said parents)
    
    The tests reflect these cases.

--- api/server.go
+++ api/server.go
@@ -344,6 +344,7 @@ func NewApiServer(config config.Config) *ApiServer {
 		g.Get("/tracks/:trackId/stems", app.v1TrackStems)
 		g.Get("/tracks/:trackId/favorites", app.v1TrackFavorites)
 		g.Get("/tracks/:trackId/comments", app.v1TrackComments)
+		g.Get("/tracks/:trackId/remixing", app.v1TrackRemixing)
 
 		// Playlists
 		g.Get("/playlists", app.v1Playlists)
