cfadfe7baa431713a3283e7d3de4d1d8737e6b24
Raymond Jacobson <ray@audius.co>
2025-06-18 14:51:12 -0700

    Add other tests

--- api/v1_users_favorites_test.go
+++ api/v1_users_favorites_test.go
@@ -12,6 +12,7 @@ func TestGetUserFavorites(t *testing.T) {
 	status, body := testGet(t, app, "/v1/users/"+trashid.MustEncodeHashID(1)+"/favorites")
 	assert.Equal(t, 200, status)
 	jsonAssert(t, body, map[string]any{
+		"data.#":                  "1",
 		"data.0.favorite_item_id": "100",
 		"data.0.favorite_type":    "SaveType.track",
 		"data.0.user_id":          "1",
@@ -29,10 +30,18 @@ func TestGetUserFavoritesEmpty(t *testing.T) {
 
 func TestGetUserFavoritesQueryParams(t *testing.T) {
 	app := testAppWithFixtures(t)
-	status, body := testGet(t, app, "/v1/users/"+trashid.MustEncodeHashID(1)+"/favorites?limit=10&offset=0")
+	status, body := testGet(t, app, "/v1/users/"+trashid.MustEncodeHashID(1)+"/favorites?limit=1&offset=0")
 	assert.Equal(t, 200, status)
 	jsonAssert(t, body, map[string]any{
+		"data.#":                  "1",
 		"data.0.favorite_item_id": "100",
 		"data.0.favorite_type":    "SaveType.track",
+		"data.0.user_id":          "1",
 	})
+}
+
+func TestGetUserFavoritesInvalidQueryParams(t *testing.T) {
+	app := testAppWithFixtures(t)
+	status, _ := testGet(t, app, "/v1/users/"+trashid.MustEncodeHashID(1)+"/favorites?limit=invalid&offset=-1")
+	assert.Equal(t, 400, status)
 }
