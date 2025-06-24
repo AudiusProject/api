d4d9a90e63d4b99e5d40eb1c7853b71320bbf8db
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-06-24 01:11:54 -0700

    Temporarily enable indexer

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+
 	"bridgerton.audius.co/api"
 	"bridgerton.audius.co/config"
+	"bridgerton.audius.co/solana/indexers"
 )
 
 func main() {
+	tokenIndexer := indexers.NewSolanaIndexer(config.Cfg)
+	go tokenIndexer.Start(context.Background())
+
 	as := api.NewApiServer(config.Cfg)
 	as.Serve()
 }
