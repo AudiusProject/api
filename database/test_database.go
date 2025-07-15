99f7cd3a54ef7642ee506012e1338fa85b37fddd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-14 23:25:45 -0700

    prevent race conditions

--- database/test_database.go
+++ database/test_database.go
@@ -15,19 +15,23 @@ var testMutex = sync.Mutex{}
 var testPoolForCreatingChildDatabases *pgxpool.Pool
 
 func NewTestDatabase(t *testing.T) *pgxpool.Pool {
+	t.Helper()
+	t.Parallel()
+
 	ctx := context.Background()
 	var err error
 
-	if testPoolForCreatingChildDatabases == nil {
-		testPoolForCreatingChildDatabases, err = pgxpool.New(ctx, "postgres://postgres:example@localhost:21300/test01")
+	dbName := fmt.Sprintf("testdb_%d", rand.Int())
+	{
+		testMutex.Lock()
+		defer testMutex.Unlock()
+		if testPoolForCreatingChildDatabases == nil {
+			testPoolForCreatingChildDatabases, err = pgxpool.New(ctx, "postgres://postgres:example@localhost:21300/test01")
+			require.NoError(t, err)
+		}
+		_, err = testPoolForCreatingChildDatabases.Exec(ctx, "CREATE DATABASE "+dbName+" TEMPLATE test01")
 		require.NoError(t, err)
 	}
-
-	dbName := fmt.Sprintf("testdb_%d", rand.Int())
-	testMutex.Lock()
-	_, err = testPoolForCreatingChildDatabases.Exec(ctx, "CREATE DATABASE "+dbName+" TEMPLATE test01")
-	testMutex.Unlock()
-	require.NoError(t, err)
 
 	connString := "postgres://postgres:example@localhost:21300/" + dbName
 	pool, err := pgxpool.New(t.Context(), connString)
@@ -35,10 +39,13 @@ func NewTestDatabase(t *testing.T) *pgxpool.Pool {
 
 	t.Cleanup(func() {
 		pool.Close()
+
 		testMutex.Lock()
-		_, err := testPoolForCreatingChildDatabases.Exec(ctx, "DROP DATABASE IF EXISTS test")
-		testMutex.Unlock()
+		defer testMutex.Unlock()
+
+		_, err := testPoolForCreatingChildDatabases.Exec(ctx, "DROP DATABASE IF EXISTS "+dbName)
 		require.NoError(t, err)
+
 	})
 	return pool
 }
