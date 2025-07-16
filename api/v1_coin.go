8204fdd4dfa6c7b022f6304d9498a837494a01d6
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-16 10:44:22 -0700

    updates coin endpoint to match coins

--- api/v1_coin.go
+++ api/v1_coin.go
@@ -16,37 +16,52 @@ func (app *ApiServer) v1Coin(c *fiber.Ctx) error {
 	sql := `
 		WITH member_counts_24h_ago AS (
 			SELECT 
-				COUNT(*) AS members,
+				COUNT(DISTINCT balances_24h_ago.user_id) AS members,
 				balances_24h_ago.mint
 			FROM (
-				SELECT DISTINCT ON (sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account)
+				SELECT DISTINCT ON (sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account, users.user_id, associated_wallets.user_id)
 					sol_token_account_balance_changes.mint,
 					sol_token_account_balance_changes.account,
-					sol_token_account_balance_changes.balance
+					sol_token_account_balance_changes.balance,
+					COALESCE(associated_wallets.user_id, users.user_id) AS user_id
 				FROM sol_token_account_balance_changes
+				LEFT JOIN associated_wallets ON associated_wallets.wallet = sol_token_account_balance_changes.owner
+				LEFT JOIN sol_claimable_accounts ON sol_claimable_accounts.account = sol_token_account_balance_changes.account
+				LEFT JOIN users ON users.wallet = sol_claimable_accounts.ethereum_address
 				WHERE block_timestamp < NOW() - INTERVAL '24 hours'
-				ORDER BY sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account, block_timestamp DESC	
+					AND (associated_wallets.user_id IS NOT NULL OR users.user_id IS NOT NULL)
+					AND sol_token_account_balance_changes.mint = @mint
+				ORDER BY sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account, users.user_id, associated_wallets.user_id, block_timestamp DESC
 			) AS balances_24h_ago
 			WHERE balance > 0
 			GROUP BY balances_24h_ago.mint
 		), member_counts AS (
 			SELECT
-				sol_token_account_balances.mint,
-				COUNT(*) AS members
-			FROM sol_token_account_balances
-			WHERE sol_token_account_balances.balance > 0
-			GROUP BY sol_token_account_balances.mint
+				member_balances.mint,
+				COUNT(DISTINCT member_balances.user_id) AS members
+			FROM (
+				SELECT sol_token_account_balances.mint,
+					COALESCE(associated_wallets.user_id, users.user_id) AS user_id
+				FROM sol_token_account_balances
+				LEFT JOIN associated_wallets ON associated_wallets.wallet = sol_token_account_balances.owner
+				LEFT JOIN sol_claimable_accounts ON sol_claimable_accounts.account = sol_token_account_balances.account
+				LEFT JOIN users ON users.wallet = sol_claimable_accounts.ethereum_address
+				WHERE sol_token_account_balances.balance > 0
+					AND (associated_wallets.wallet IS NOT NULL OR sol_claimable_accounts.account IS NOT NULL)
+					AND sol_token_account_balances.mint = @mint
+			) AS member_balances
+			GROUP BY member_balances.mint
 		)
 		SELECT 
 			artist_coins.ticker,
 			artist_coins.mint,
 			artist_coins.user_id,
 			artist_coins.created_at,
-			member_counts.members,
-			((member_counts.members - member_counts_24h_ago.members) * 100.0 / NULLIF(member_counts_24h_ago.members, 0)) AS members_24h_change_percent
+			COALESCE(member_counts.members, 0) AS members,
+			((COALESCE(member_counts.members, 0) - member_counts_24h_ago.members) * 100.0 / NULLIF(member_counts_24h_ago.members, 0)) AS members_24h_change_percent
 		FROM artist_coins
-		JOIN member_counts ON artist_coins.mint = member_counts.mint
-		JOIN member_counts_24h_ago ON artist_coins.mint = member_counts_24h_ago.mint
+		LEFT JOIN member_counts ON artist_coins.mint = member_counts.mint
+		LEFT JOIN member_counts_24h_ago ON artist_coins.mint = member_counts_24h_ago.mint
 		WHERE artist_coins.mint = @mint
 		LIMIT 1
 	`
