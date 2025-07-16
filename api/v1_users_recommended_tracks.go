4795e96228d74956310ffc95139d1189e74b7024
Raymond Jacobson <ray@audius.co>
2025-07-16 15:46:49 -0700

    Fix spacing

--- api/v1_users_recommended_tracks.go
+++ api/v1_users_recommended_tracks.go
@@ -32,52 +32,52 @@ func (app *ApiServer) v1UsersRecommendedTracks(c *fiber.Ctx) error {
 	// filtering out already played tracks and picking randomly from them.
 	sql := `
 		WITH played_tracks AS (
-	  SELECT DISTINCT play_item_id
-	  FROM plays
-	  WHERE user_id = @userId
-	),
-	top_genres AS (
-	  SELECT t.genre
-	  FROM played_tracks pt
-	  JOIN tracks t ON t.track_id = pt.play_item_id
-	  WHERE t.genre IS NOT NULL
-	  GROUP BY t.genre
-	  ORDER BY COUNT(*) DESC
-	  LIMIT 5
-	),
-	top_tracks_per_genre AS (
-	  SELECT
-	    tg.genre,
-	    tts.track_id,
-	    tts.score
-	  FROM top_genres tg
-	  JOIN LATERAL (
-	    SELECT tts.track_id, tts.score
-	    FROM (
-	      SELECT tts.track_id, tts.score
-	      FROM track_trending_scores tts
-	      WHERE tts.genre = tg.genre
-	        AND tts.time_range = @timeRange
-					AND tts.version = 'pnagD'
-	      ORDER BY tts.score DESC
-				-- Limit the number of tracks we recall to improve performance
-	      LIMIT 3000
-	    ) tts
-	    WHERE NOT EXISTS (
-	      SELECT 1 FROM played_tracks pt WHERE pt.play_item_id = tts.track_id
-	    )
+			SELECT DISTINCT play_item_id
+			FROM plays
+			WHERE user_id = @userId
+		),
+		top_genres AS (
+			SELECT t.genre
+			FROM played_tracks pt
+			JOIN tracks t ON t.track_id = pt.play_item_id
+			WHERE t.genre IS NOT NULL
+			GROUP BY t.genre
+			ORDER BY COUNT(*) DESC
+			LIMIT 5
+		),
+		top_tracks_per_genre AS (
+			SELECT
+				tg.genre,
+				tts.track_id,
+				tts.score
+			FROM top_genres tg
+			JOIN LATERAL (
+				SELECT tts.track_id, tts.score
+				FROM (
+					SELECT tts.track_id, tts.score
+					FROM track_trending_scores tts
+					WHERE tts.genre = tg.genre
+						AND tts.time_range = @timeRange
+						AND tts.version = 'pnagD'
+					ORDER BY tts.score DESC
+					-- Limit the number of tracks we recall to improve performance
+					LIMIT 3000
+				) tts
+				WHERE NOT EXISTS (
+					SELECT 1 FROM played_tracks pt WHERE pt.play_item_id = tts.track_id
+				)
+			LIMIT 10
+			) tts ON true
+		)
+		SELECT t.track_id
+		FROM top_tracks_per_genre
+		JOIN tracks t ON top_tracks_per_genre.track_id = t.track_id
+		WHERE
+			t.is_unlisted = false
+			AND t.is_current = true
+			AND t.is_delete = false
+		ORDER BY random()
 		LIMIT 10
-	  ) tts ON true
-	)
-	SELECT t.track_id
-	FROM top_tracks_per_genre
-	JOIN tracks t ON top_tracks_per_genre.track_id = t.track_id
-	WHERE
-		t.is_unlisted = false
-		AND t.is_current = true
-		AND t.is_delete = false
-	ORDER BY random()
-	LIMIT 10
 	`
 
 	rows, err := app.pool.Query(c.Context(), sql, pgx.NamedArgs{
