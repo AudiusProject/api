801b17a1c643aed012f898399f6a0b8f9a2f98c8
Steve Perkins <stereosteve@users.noreply.github.com>
2025-07-18 10:51:10 -0400

    more tests

--- api/v1_notifications_test.go
+++ api/v1_notifications_test.go
@@ -38,11 +38,17 @@ func TestV1Notifications(t *testing.T) {
 
 	jsonAssert(t, body, map[string]any{
 		"data.notifications.0.type":                            "tip_send",
+		"data.notifications.0.actions.0.specifier":             "D2Wde",
 		"data.notifications.0.actions.0.data.amount":           "1000000000000000000",
 		"data.notifications.0.actions.0.data.tip_tx_signature": "asdf",
+		"data.notifications.0.actions.0.data.sender_user_id":   "D91oD",
+		"data.notifications.0.actions.0.data.receiver_user_id": "n0AML",
 
-		"data.notifications.1.actions.0.data.type":     "playlist_repost_count",
-		"data.notifications.1.actions.0.data.is_album": false,
+		"data.notifications.1.type":                       "milestone",
+		"data.notifications.1.actions.0.specifier":        "4W2ay",
+		"data.notifications.1.actions.0.data.type":        "playlist_repost_count",
+		"data.notifications.1.actions.0.data.is_album":    false,
+		"data.notifications.1.actions.0.data.playlist_id": "WQ2P9",
 	})
 
 }
