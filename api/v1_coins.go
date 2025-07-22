b6518cb52b7f61d8c01f34229a86de4a16c913ed
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-22 11:34:14 -0700

    ray called my queries fat

--- api/v1_coins.go
+++ api/v1_coins.go
@@ -12,6 +12,7 @@ import (
 )
 
 type GetArtistCoinsQueryParams struct {
+	Tickers  []string         `query:"ticker"`
 	Mints    []string         `query:"mint"`
 	OwnerIds []trashid.HashId `query:"owner_id"`
 	Limit    int              `query:"limit" default:"50" validate:"min=1,max=100"`
@@ -42,6 +43,10 @@ func (app *ApiServer) v1Coins(c *fiber.Ctx) error {
 	if len(queryParams.OwnerIds) > 0 {
 		ownerIdFilter = `AND artist_coins.user_id = ANY(@owner_ids)`
 	}
+	tickerFilter := ""
+	if len(queryParams.Tickers) > 0 {
+		tickerFilter = `AND artist_coins.ticker = ANY(@tickers)`
+	}
 
 	sql := `
 		WITH member_counts_24h_ago AS (
@@ -49,18 +54,30 @@ func (app *ApiServer) v1Coins(c *fiber.Ctx) error {
 				COUNT(DISTINCT balances_24h_ago.user_id) AS members,
 				balances_24h_ago.mint
 			FROM (
-				SELECT DISTINCT ON (sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account, users.user_id, associated_wallets.user_id)
+				SELECT DISTINCT ON (
+						sol_token_account_balance_changes.mint, 
+						sol_token_account_balance_changes.account, 
+						users.user_id, 
+						associated_wallets.user_id
+					)
 					sol_token_account_balance_changes.mint,
 					sol_token_account_balance_changes.account,
 					sol_token_account_balance_changes.balance,
 					COALESCE(associated_wallets.user_id, users.user_id) AS user_id
 				FROM sol_token_account_balance_changes
-				LEFT JOIN associated_wallets ON associated_wallets.wallet = sol_token_account_balance_changes.owner
-				LEFT JOIN sol_claimable_accounts ON sol_claimable_accounts.account = sol_token_account_balance_changes.account
+				LEFT JOIN associated_wallets 
+					ON associated_wallets.wallet = sol_token_account_balance_changes.owner
+				LEFT JOIN sol_claimable_accounts 
+					ON sol_claimable_accounts.account = sol_token_account_balance_changes.account
 				LEFT JOIN users ON users.wallet = sol_claimable_accounts.ethereum_address
 				WHERE block_timestamp < NOW() - INTERVAL '24 hours'
 					AND (associated_wallets.user_id IS NOT NULL OR users.user_id IS NOT NULL)
-				ORDER BY sol_token_account_balance_changes.mint, sol_token_account_balance_changes.account, users.user_id, associated_wallets.user_id, block_timestamp DESC
+				ORDER BY 
+					sol_token_account_balance_changes.mint, 
+					sol_token_account_balance_changes.account, 
+					users.user_id, 
+					associated_wallets.user_id, 
+					block_timestamp DESC
 			) AS balances_24h_ago
 			WHERE balance > 0
 			GROUP BY balances_24h_ago.mint
@@ -72,11 +89,15 @@ func (app *ApiServer) v1Coins(c *fiber.Ctx) error {
 				SELECT sol_token_account_balances.mint,
 					COALESCE(associated_wallets.user_id, users.user_id) AS user_id
 				FROM sol_token_account_balances
-				LEFT JOIN associated_wallets ON associated_wallets.wallet = sol_token_account_balances.owner
-				LEFT JOIN sol_claimable_accounts ON sol_claimable_accounts.account = sol_token_account_balances.account
-				LEFT JOIN users ON users.wallet = sol_claimable_accounts.ethereum_address
+				LEFT JOIN associated_wallets 
+					ON associated_wallets.wallet = sol_token_account_balances.owner
+				LEFT JOIN sol_claimable_accounts 
+					ON sol_claimable_accounts.account = sol_token_account_balances.account
+				LEFT JOIN users 
+					ON users.wallet = sol_claimable_accounts.ethereum_address
 				WHERE sol_token_account_balances.balance > 0
-					AND (associated_wallets.wallet IS NOT NULL OR sol_claimable_accounts.account IS NOT NULL)
+					AND (associated_wallets.wallet IS NOT NULL 
+							OR sol_claimable_accounts.account IS NOT NULL)
 			) AS member_balances
 			GROUP BY member_balances.mint
 		)
@@ -86,19 +107,24 @@ func (app *ApiServer) v1Coins(c *fiber.Ctx) error {
 			artist_coins.user_id,
 			artist_coins.created_at,
 			COALESCE(member_counts.members, 0) AS members,
-			((COALESCE(member_counts.members, 0) - member_counts_24h_ago.members) * 100.0 / NULLIF(member_counts_24h_ago.members, 0)) AS members_24h_change_percent
+			(
+				(COALESCE(member_counts.members, 0) - member_counts_24h_ago.members) * 100.0 /
+				NULLIF(member_counts_24h_ago.members, 0)
+			) AS members_24h_change_percent
 		FROM artist_coins
 		LEFT JOIN member_counts ON artist_coins.mint = member_counts.mint
 		LEFT JOIN member_counts_24h_ago ON artist_coins.mint = member_counts_24h_ago.mint
 		WHERE 1=1
 			` + mintFilter + `
 			` + ownerIdFilter + `
+			` + tickerFilter + `
 		ORDER BY artist_coins.created_at ASC
 		LIMIT @limit
 		OFFSET @offset
 	`
 
 	rows, err := app.pool.Query(c.Context(), sql, pgx.NamedArgs{
+		"tickers":   queryParams.Tickers,
 		"mints":     queryParams.Mints,
 		"owner_ids": queryParams.OwnerIds,
 		"limit":     queryParams.Limit,
