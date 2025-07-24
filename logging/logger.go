7716de266c60680fad9fa63f51c56b23efd908dd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-24 15:30:42 -0700

    Only log info to axiom, only log unexpected errors

--- logging/logger.go
+++ logging/logger.go
@@ -55,6 +55,7 @@ func NewZapLogger(config config.Config) *zap.Logger {
 			),
 			adapter.SetDataset(config.AxiomDataset),
 		)
+		axiomAdapter.Enabled(zap.InfoLevel)
 		if err != nil {
 			panic(err)
 		}
@@ -76,7 +77,13 @@ func SyncOnTicks(ctx context.Context, logger *zap.Logger, tickDuration time.Dura
 			return ctx.Err()
 		case <-ticker.C:
 			if err := logger.Sync(); err != nil {
-				logger.Error("failed to sync logger on tick", zap.Error(err))
+				// This specific error occurs when running in a container
+				// where /dev/stdout is not a valid file descriptor.
+				// We can ignore it since it doesn't affect logging to Axiom.
+				// See: https://github.com/uber-go/zap/issues/328#issuecomment-284337436
+				if err != os.ErrInvalid {
+					logger.Error("failed to sync logger on tick", zap.Error(err))
+				}
 			}
 		}
 	}
