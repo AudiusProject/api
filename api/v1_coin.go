1175fcb08f03208b5a720386c1f7689f3f0c5696
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-30 02:17:31 -0700

    Add back v1Coin

---
+++ api/v1_coin.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+)
+
+func (app *ApiServer) v1Coin(c *fiber.Ctx) error {
+	mint := c.Params("mint")
+	if mint == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "mint parameter is required",
+		})
+	}
+
+	sql := `
+		SELECT 
+			artist_coins.ticker,
+			artist_coins.mint,
+			artist_coins.decimals,
+			artist_coins.user_id,
+			artist_coins.created_at
+		FROM artist_coins
+		WHERE artist_coins.mint = @mint
+		LIMIT 1
+	`
+
+	rows, err := app.pool.Query(c.Context(), sql, pgx.NamedArgs{
+		"mint": mint,
+	})
+	if err != nil {
+		return err
+	}
+
+	coinRow, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[ArtistCoin])
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"data": coinRow,
+	})
+}
