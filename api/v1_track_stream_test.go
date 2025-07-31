3a669ddbd412bdf6e8936656f65ab568df24da2f
Alec Savvy <alecsavvy@outlook.com>
2025-07-31 11:13:18 -0600

    fix test

--- api/v1_track_stream_test.go
+++ api/v1_track_stream_test.go
@@ -12,5 +12,5 @@ func TestGetTrackStream(t *testing.T) {
 	req := httptest.NewRequest("GET", "/v1/tracks/eYJyn/stream", nil)
 	res, err := app.Test(req, -1)
 	assert.NoError(t, err)
-	assert.Contains(t, res.Header.Get("Location"), "tracks/cidstream/?id3=false&id3_artist=&id3_title=Culca+Canyon&signature=%7B%22data%22%3A%22%7B%5C%22cid%5C%22%3A%5C%22%5C%22%2C%5C%22timestamp%5C%22%3")
+	assert.Contains(t, res.Header.Get("Location"), "tracks/cidstream/?id3=true&id3_artist=&id3_title=Culca+Canyon&signature=%7B%22data%22%3A%22%7B%5C%22cid%5C%22%3A%5C%22%5C%22%2C%5C%22timestamp%5C%22%3")
 }
