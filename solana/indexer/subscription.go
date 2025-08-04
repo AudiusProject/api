1cf54a24a17149c7ae4f5664733ff4d538636b77
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-08-04 09:29:42 -0700

    Solana Indexer: Process messages in parallel (#292)
    - Adds parallelism to solana-indexer.
      - Defaults to 50 workers for stage and prod and 1 for local
    - Tested with 50 workers locally and indexed USDC, wAUDIO, BONK, WEN and
    was able to catch up from behind 3000 slots. Was seeing numbers of
    around 50k account balance changes indexed per minute with this many
    workers. We'll see how our cloud sql db handles this - can always tune
    the worker count - but this should give us plenty of headroom I think.
    - Reduces slot checkpoint writes to every 10 slots and only if the slot
    is greater than the current one to help reduce load.
    - Bails early in processing if found signature in the cache, as it's
    already processed then.
    - Removes sql transaction that wasn't doing anything since it wasn't
    wired up properly. (Fixing the code to use it causes deadlocks as the
    concurrency causes the inserts to compete for locks on tables I guess?)

--- solana/indexer/subscription.go
+++ solana/indexer/subscription.go
@@ -24,7 +24,31 @@ type artistCoinsChangedNotification struct {
 }
 
 func (s *SolanaIndexer) Subscribe(ctx context.Context) error {
+	// Set up workers to process updates concurrently
+	msgChan := make(chan *pb.SubscribeUpdate, 3000)
+	for i := range s.workerCount {
+		go func(workerId int32) {
+			for msg := range msgChan {
+				s.handleMessage(ctx, msg)
+			}
+		}(i)
+	}
+	defer close(msgChan)
+
+	// On a new message, queue the message to the worker pool
+	onMessage := func(ctx context.Context, msg *pb.SubscribeUpdate) {
+		select {
+		case <-ctx.Done():
+			s.logger.Warn("subscription context cancelled, stopping message processing")
+			return
+		case msgChan <- msg:
+		}
+	}
+
+	// Flush the logger every 15 seconds to ensure logs are written out
 	go logging.SyncOnTicks(ctx, s.logger, time.Second*15)
+
+	// Acquire a connection to the database and listen for artist coins changes
 	conn, err := s.pool.Acquire(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to acquire database connection: %w", err)
@@ -37,10 +61,12 @@ func (s *SolanaIndexer) Subscribe(ctx context.Context) error {
 		return fmt.Errorf("failed to listen for artist coins changes: %w", err)
 	}
 
+	// Log when we receive a shutdown signal
 	defer func() {
 		s.logger.Info("received shutdown signal, stopping subscription")
 	}()
 
+	// Loop to reset subscription when the artist coins notification is received
 	for {
 		select {
 		case <-ctx.Done():
@@ -57,6 +83,9 @@ func (s *SolanaIndexer) Subscribe(ctx context.Context) error {
 		if err != nil {
 			return fmt.Errorf("failed to create subscription: %w", err)
 		}
+
+		// Check if a backfill is needed with the new subscription
+		// and find the slot to continue from.
 
 		lastIndexedSlot, err := getCheckpointSlot(ctx, s.pool, subscription)
 		if err != nil {
@@ -101,7 +130,8 @@ func (s *SolanaIndexer) Subscribe(ctx context.Context) error {
 
 		subCtx, cancel := context.WithCancel(ctx)
 		defer cancel()
-		if err := s.grpcClient.Subscribe(subCtx, subscription, s.onMessage, s.onError); err != nil {
+
+		if err := s.grpcClient.Subscribe(subCtx, subscription, onMessage, s.onError); err != nil {
 			return fmt.Errorf("failed to subscribe to gRPC server: %w", err)
 		}
 
@@ -125,7 +155,7 @@ func (s *SolanaIndexer) Subscribe(ctx context.Context) error {
 					continue
 				}
 				if notifData.Operation != "INSERT" && notifData.Operation != "DELETE" {
-					// ignore updates
+					// ignore updates - only care if mints are added or removed
 					continue
 				}
 				s.logger.Info("artist_coins changed, re-starting subscription",
@@ -197,13 +227,17 @@ func buildSubscriptionRequest(mintAddresses []string) (*pb.SubscribeRequest, err
 }
 
 // Handles a message from the gRPC subscription.
-func (s *SolanaIndexer) onMessage(ctx context.Context, msg *pb.SubscribeUpdate) {
+func (s *SolanaIndexer) handleMessage(ctx context.Context, msg *pb.SubscribeUpdate) {
 	logger := s.logger.With(zap.String("indexerSource", "grpc"))
 
 	if slotUpdate := msg.GetSlot(); slotUpdate != nil && slotUpdate.Slot > 0 {
-		err := updateCheckpoint(ctx, s.pool, s.checkpointId, slotUpdate.Slot)
-		if err != nil {
-			logger.Error("failed to update slot checkpoint", zap.Error(err))
+		// only update every 10 slots to reduce db load and write latency
+		if slotUpdate.Slot%10 == 0 {
+			s.logger.Debug("slot update", zap.Uint64("slot", slotUpdate.Slot))
+			err := updateCheckpoint(ctx, s.pool, s.checkpointId, slotUpdate.Slot)
+			if err != nil {
+				logger.Error("failed to update slot checkpoint", zap.Error(err))
+			}
 		}
 	}
 
@@ -215,7 +249,6 @@ func (s *SolanaIndexer) onMessage(ctx context.Context, msg *pb.SubscribeUpdate) 
 			logger.Error("failed to process signature", zap.Error(err))
 		}
 	}
-
 }
 
 func (s *SolanaIndexer) onError(err error) {
