571b7fab2451a17929b5b39af91ff2af23e91015
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-09-24 14:03:33 -0400

    fix some test failures after merging main

--- api/dbv1/models.go
+++ api/dbv1/models.go
@@ -846,7 +846,6 @@ type ArtistCoin struct {
 	Description pgtype.Text `json:"description"`
 	Website     pgtype.Text `json:"website"`
 	Name        string      `json:"name"`
-	DbcPool     pgtype.Text `json:"dbc_pool"`
 	HasDiscord  bool        `json:"has_discord"`
 }
 
