9451dfc4b0787cfa56c87b16da09e2387391169a
Raymond Jacobson <ray@audius.co>
2025-10-03 17:35:12 -0700

    Better yet

--- ddl/migrations/0168_sol_keypairs.sql
+++ ddl/migrations/0168_sol_keypairs.sql
@@ -1,9 +1,8 @@
 begin;
 
 create table sol_keypairs (
-    id bigint primary key,
-    public_key varchar not null,
-    private_key varchar not null
+    public_key varchar primary key,
+    private_key bytea not null
 );
 
 commit;
