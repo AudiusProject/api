e0a8dc1242e9cd301f6568fe0ecd3c9634af3988
JD Francis <jd@audius.co>
2025-10-05 20:35:39 -0500

    Adds creatorWalletAddress to /coin endpoint

--- api/v1_coin.go
+++ api/v1_coin.go
@@ -75,7 +75,8 @@ func (app *ApiServer) v1Coin(c *fiber.Ctx) error {
 				'curveProgress', COALESCE(artist_coin_pools.curve_progress, 0),
 				'isMigrated', COALESCE(artist_coin_pools.is_migrated, false),
 				'creatorQuoteFee', COALESCE(artist_coin_pools.creator_quote_fee, 0),
-				'totalTradingQuoteFee', COALESCE(artist_coin_pools.total_trading_quote_fee, 0)
+				'totalTradingQuoteFee', COALESCE(artist_coin_pools.total_trading_quote_fee, 0),
+				'creatorWalletAddress', COALESCE(artist_coin_pools.creator_wallet_address, '')
 			) AS dynamic_bonding_curve,
 			COALESCE(artist_coin_stats.updated_at, artist_coins.created_at) as updated_at
 		FROM artist_coins
@@ -174,7 +175,8 @@ func (app *ApiServer) v1CoinByTicker(c *fiber.Ctx) error {
 				'curveProgress', COALESCE(artist_coin_pools.curve_progress, 0),
 				'isMigrated', COALESCE(artist_coin_pools.is_migrated, false),
 				'creatorQuoteFee', COALESCE(artist_coin_pools.creator_quote_fee, 0),
-				'totalTradingQuoteFee', COALESCE(artist_coin_pools.total_trading_quote_fee, 0)
+				'totalTradingQuoteFee', COALESCE(artist_coin_pools.total_trading_quote_fee, 0),
+				'creatorWalletAddress', COALESCE(artist_coin_pools.creator_wallet_address, '')
 			) AS dynamic_bonding_curve,
 			COALESCE(artist_coin_stats.updated_at, artist_coins.created_at) as updated_at
 		FROM artist_coins
