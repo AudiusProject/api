fc9f8b8f59c61b9c40c338ca09d3246d9f49dcad
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-10-23 10:56:14 -0400

    not in is cooler

--- api/dbv1/queries/get_users.sql
+++ api/dbv1/queries/get_users.sql
@@ -177,9 +177,11 @@ SELECT
           JOIN artist_coins ON artist_coins.mint = sol_user_balances.mint -- ensure mapped in artist_coins
           WHERE sol_user_balances.user_id = u.user_id
             AND sol_user_balances.balance > 0
-            AND sol_user_balances.mint != '9LzCMqDgTKYz9Drzqnpgee3SGa89up3a247ypMj2xrqM' -- ignore prod wAUDIO
-            AND sol_user_balances.mint != 'BELGiMZQ34SDE6x2FUaML2UHDAgBLS64xvhXjX5tBBZo' -- ignore stage wAUDIO
-            AND sol_user_balances.mint != 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZy4z6cQ' -- ignore USDC
+            AND sol_user_balances.mint NOT IN (
+              '9LzCMqDgTKYz9Drzqnpgee3SGa89up3a247ypMj2xrqM', -- ignore prod wAUDIO
+              'BELGiMZQ34SDE6x2FUaML2UHDAgBLS64xvhXjX5tBBZo', -- ignore stage wAUDIO
+              'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZy4z6cQ' -- ignore USDC
+            )
           ORDER BY sol_user_balances.balance DESC
           LIMIT 1
         )
