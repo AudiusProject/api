021b74dddc24c78bfac7d12111f8dcece1e3c94a
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-10-24 19:10:12 -0400

    comments on indexes

--- ddl/migrations/0175_add_user_score_tables.sql
+++ ddl/migrations/0175_add_user_score_tables.sql
@@ -1,7 +1,7 @@
 begin;
 
--- Create index for plays by user ID and hourly timestamp
 CREATE INDEX IF NOT EXISTS ix_plays_user_hour ON plays (user_id, date_trunc('hour', created_at)) WHERE user_id IS NOT NULL;
+COMMENT ON INDEX ix_plays_user_hour IS 'Helps compute distinct hourly plays by user id';
 
 
 CREATE TABLE IF NOT EXISTS user_distinct_play_hours (
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS user_score_features (
 COMMENT ON TABLE user_score_features IS 'Tracks some features used in user score calculation';
 COMMENT ON COLUMN user_score_features.challenge_count IS 'Tracks the number of fast challenges auser has completed';
 
--- Helps with karma calculation in user score queries
 CREATE INDEX IF NOT EXISTS ix_au_user_follows
   ON aggregate_user (user_id)
   INCLUDE (follower_count, following_count);
+COMMENT ON INDEX ix_au_user_follows IS 'Fast lookup for fields use in karma calculation';
 
 COMMIT;
