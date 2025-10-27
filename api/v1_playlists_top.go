4279103262694e571dcfe786cbbc3a1db00d3205
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-10-27 17:04:46 -0400

    remove unnecessary double processing

--- api/v1_playlists_top.go
+++ api/v1_playlists_top.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"strings"
-
 	"api.audius.co/api/dbv1"
 	"api.audius.co/api/searchv1"
 	"github.com/gofiber/fiber/v2"
@@ -23,9 +21,7 @@ func (app *ApiServer) v1PlaylistsTop(c *fiber.Ctx) error {
 
 	moods := []string{}
 	if params.Mood != "" {
-		// Working around a quirk with mood indexing: track moods begin with a capital letter and must match exactly.
-		capitalizedMood := strings.ToUpper(string(params.Mood[0])) + strings.ToLower(params.Mood[1:])
-		moods = []string{capitalizedMood}
+		moods = []string{params.Mood}
 	}
 
 	q := searchv1.PlaylistSearchQuery{
