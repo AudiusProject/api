4279187bf6397112b2f9cd40c5a92dc5a3059c92
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-11-11 13:00:07 -0500

    Don't add challenge notifs for redeeming coin reward codes (#563)
    We're generating notifications when challenge disbursements get inserted
    for redeeming a code. This adds a check for a matching code against the
    challenge_id and skips adding the notification.

--- ddl/functions/handle_challenge_disbursements.sql
+++ ddl/functions/handle_challenge_disbursements.sql
@@ -2,34 +2,40 @@ create or replace function handle_challenge_disbursement() returns trigger as $$
 declare
   reward_manager_tx reward_manager_txs%ROWTYPE;
 	existing_notification integer;
+	reward_code_exists boolean;
 begin
 
   select * into reward_manager_tx from reward_manager_txs where reward_manager_txs.signature = new.signature limit 1;
 
   if reward_manager_tx is not null then
-		select id into existing_notification 
-		from notification
-		where
-		type = 'challenge_reward' and
-		new.user_id = any(user_ids) and
-		timestamp >= (new.created_at - interval '1 hour')
-		limit 1;
-		
-		if existing_notification is null then
-			-- create a notification for the challenge disbursement
-			insert into notification
-			(slot, user_ids, timestamp, type, group_id, specifier, data)
-			values
-			(
-				new.slot,
-				ARRAY [new.user_id],
-				new.created_at,
-				'challenge_reward',
-				'challenge_reward:' || new.user_id || ':challenge:' || new.challenge_id || ':specifier:' || new.specifier,
-				new.user_id,
-				json_build_object('specifier', new.specifier, 'challenge_id', new.challenge_id, 'amount', new.amount)
-			)
-			on conflict do nothing;
+		-- check if reward_codes table has an entry where code equals new.challenge_id
+		select exists(select 1 from reward_codes where code = new.challenge_id) into reward_code_exists;
+
+		if not reward_code_exists then
+			select id into existing_notification
+			from notification
+			where
+			type = 'challenge_reward' and
+			new.user_id = any(user_ids) and
+			timestamp >= (new.created_at - interval '1 hour')
+			limit 1;
+
+			if existing_notification is null then
+				-- create a notification for the challenge disbursement
+				insert into notification
+				(slot, user_ids, timestamp, type, group_id, specifier, data)
+				values
+				(
+					new.slot,
+					ARRAY [new.user_id],
+					new.created_at,
+					'challenge_reward',
+					'challenge_reward:' || new.user_id || ':challenge:' || new.challenge_id || ':specifier:' || new.specifier,
+					new.user_id,
+					json_build_object('specifier', new.specifier, 'challenge_id', new.challenge_id, 'amount', new.amount)
+				)
+				on conflict do nothing;
+			end if;
 		end if;
   end if;
   return null;
