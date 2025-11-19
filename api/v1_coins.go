4e8f2455574cb9db240c2c5b5a7a68f6ba8ba912
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-11-19 14:54:26 -0500

    Add escrow recipient to coins response

--- api/v1_coins.go
+++ api/v1_coins.go
@@ -10,21 +10,22 @@ import (
 )
 
 type ArtistCoin struct {
-	Name           string         `json:"name"`
-	Ticker         string         `json:"ticker"`
-	Mint           string         `json:"mint"`
-	Decimals       int            `json:"decimals"`
-	OwnerId        trashid.HashId `db:"user_id" json:"owner_id"`
-	LogoUri        *string        `json:"logo_uri,omitempty"`
-	BannerImageUrl *string        `json:"banner_image_url,omitempty"`
-	Description    *string        `json:"description,omitempty"`
-	Link1          *string        `json:"link_1,omitempty"`
-	Link2          *string        `json:"link_2,omitempty"`
-	Link3          *string        `json:"link_3,omitempty"`
-	Link4          *string        `json:"link_4,omitempty"`
-	HasDiscord     bool           `json:"has_discord"`
-	CreatedAt      time.Time      `json:"created_at"`
-	CoinUpdatedAt  time.Time      `json:"coin_updated_at"`
+	Name            string         `json:"name"`
+	Ticker          string         `json:"ticker"`
+	Mint            string         `json:"mint"`
+	Decimals        int            `json:"decimals"`
+	OwnerId         trashid.HashId `db:"user_id" json:"owner_id"`
+	EscrowRecipient *string        `json:"escrow_recipient,omitempty"`
+	LogoUri         *string        `json:"logo_uri,omitempty"`
+	BannerImageUrl  *string        `json:"banner_image_url,omitempty"`
+	Description     *string        `json:"description,omitempty"`
+	Link1           *string        `json:"link_1,omitempty"`
+	Link2           *string        `json:"link_2,omitempty"`
+	Link3           *string        `json:"link_3,omitempty"`
+	Link4           *string        `json:"link_4,omitempty"`
+	HasDiscord      bool           `json:"has_discord"`
+	CreatedAt       time.Time      `json:"created_at"`
+	CoinUpdatedAt   time.Time      `json:"coin_updated_at"`
 
 	MarketCap                    float64 `json:"marketCap" db:"market_cap"`
 	FDV                          float64 `json:"fdv" db:"fdv"`
@@ -106,6 +107,7 @@ const sharedSelectCoinSql = `
 			artist_coins.ticker,
 			artist_coins.decimals,
 			artist_coins.user_id,
+			sol_locker_vesting_escrows.recipient as escrow_recipient,
 			artist_coins.logo_uri,
 			artist_coins.banner_image_url,
 			artist_coins.description,
@@ -205,6 +207,8 @@ const sharedSelectCoinSql = `
 		LEFT JOIN sol_token_account_balances AS reward_pool
 			ON reward_pool.owner = sol_reward_manager_inits.authority
 			AND reward_pool.account != sol_reward_manager_inits.token_source
+		LEFT JOIN sol_locker_vesting_escrows
+			ON sol_locker_vesting_escrows.token_mint = artist_coins.mint
 `
 
 type GetArtistCoinsQueryParams struct {
