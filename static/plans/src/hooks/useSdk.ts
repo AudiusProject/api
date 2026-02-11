73fd8f746fe6493424d7a473b3e2960e34dab036
Raymond Jacobson <ray@audius.co>
2026-02-11 12:12:00 -0800

    Fix build

--- static/plans/src/hooks/useSdk.ts
+++ static/plans/src/hooks/useSdk.ts
@@ -1,6 +1,8 @@
 import { sdk } from "@audius/sdk";
 
-const apiKey = import.meta.env.VITE_AUDIUS_API_KEY as string;
+const apiKey =
+  (import.meta.env.VITE_AUDIUS_API_KEY as string | undefined) ??
+  "8acf5eb7436ea403ee536a7334faa5e9ada4b50f";
 
 const instance = sdk({
   appName: "Audius API Plans",
