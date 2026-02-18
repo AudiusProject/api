4d385fad66974856bba59fb40d82875eadffaa40
Dylan Jeffers <dylan@audius.co>
2026-02-17 16:41:18 -0800

    Add is_save_of_repost to track and playlist favorites

--- api/v1_track_favorites.go
+++ api/v1_track_favorites.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"strconv"
 	"time"
 
@@ -50,6 +51,17 @@ func (app *ApiServer) postV1TrackFavorite(c *fiber.Ctx) error {
 		return err
 	}
 
+	metadata := ""
+	if body := c.Body(); len(body) > 0 {
+		var reqBody struct {
+			IsSaveOfRepost *bool `json:"is_save_of_repost"`
+		}
+		if err := json.Unmarshal(body, &reqBody); err == nil && reqBody.IsSaveOfRepost != nil {
+			metadataBytes, _ := json.Marshal(map[string]bool{"is_save_of_repost": *reqBody.IsSaveOfRepost})
+			metadata = string(metadataBytes)
+		}
+	}
+
 	nonce := time.Now().UnixNano()
 
 	manageEntityTx := &corev1.ManageEntityLegacy{
@@ -59,7 +71,7 @@ func (app *ApiServer) postV1TrackFavorite(c *fiber.Ctx) error {
 		Action:     indexer.Action_Save,
 		EntityType: indexer.Entity_Track,
 		Nonce:      strconv.FormatInt(nonce, 10),
-		Metadata:   "",
+		Metadata:   metadata,
 	}
 
 	response, err := app.sendTransactionWithSigner(manageEntityTx, signer.PrivateKey)
